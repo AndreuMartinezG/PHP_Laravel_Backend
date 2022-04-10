0035b385714733e38023a502708ea1957637b272
“AndreuMartinez” <andreu.martinez.garcia.1992@gmail.com>
2022-04-10 20:00:45 +0200

    feat: Creado Controller para Users

--- routes/api.php
+++ routes/api.php
@@ -35,7 +35,7 @@ Route::group([
 ], function () {
     Route::get('/me', [AuthController::class, 'me']);
     Route::get('/users', [UserController::class, 'allUsers']);
-    Route::get('/users/{id}', [UserController::class, 'getUser']);
+    Route::get('/users/{id}', [UserController::class, 'userByID']);
     Route::put('/users/{id}', [UserController::class, 'updateUser']);
     Route::delete('/users/{id}', [UserController::class, 'deleteUser']);
 
