d27730c1461ca3e5a9721f7256cd5f386f1b7c29
“AndreuMartinez” <andreu.martinez.garcia.1992@gmail.com>
2022-04-10 18:36:46 +0200

    feat: Seeders creados para GamesSeeder.php

--- database/seeders/GamesSeeder.php
+++ database/seeders/GamesSeeder.php
@@ -13,6 +13,38 @@ class GamesSeeder extends Seeder
      */
     public function run()
     {
-        //
+
+        Game::create([
+            'title' => 'League of Legends',
+            'thumbnail_url' => 'https://esports.as.com/2021/01/22/league-of-legends/URF-ARAM_1430866911_570762_667x386.jpg',
+            'url' => 'https://www.leagueoflegends.com/es-es/'
+        ]);
+
+        Game::create([
+            'title' => 'Fortnite',
+            'thumbnail_url' => 'https://i.pinimg.com/474x/8c/e8/ab/8ce8aba0edcb78be32945243a3d9b4e6.jpg',
+            'url' => 'https://www.epicgames.com/fortnite/es-ES/home'
+        ]);
+
+        Game::create([
+            'title' => 'Warcraft III',
+            'thumbnail_url' => 'https://i.pinimg.com/474x/8c/e8/ab/8ce8aba0edcb78be32945243a3d9b4e6.jpg',
+            'url' => 'https://www.epicgames.com/fortnite/es-ES/home'
+        ]);
+
+        Game::create([
+            'title' => 'Diablo III',
+            'thumbnail_url' => 'https://i.pinimg.com/474x/8c/e8/ab/8ce8aba0edcb78be32945243a3d9b4e6.jpg',
+            'url' => 'https://www.epicgames.com/fortnite/es-ES/home'
+        ]);
+
+        Game::create([
+            'title' => 'World of Warcraft',
+            'thumbnail_url' => 'https://i.pinimg.com/474x/8c/e8/ab/8ce8aba0edcb78be32945243a3d9b4e6.jpg',
+            'url' => 'https://www.epicgames.com/fortnite/es-ES/home'
+        ]);
+
+
+
     }
 }
