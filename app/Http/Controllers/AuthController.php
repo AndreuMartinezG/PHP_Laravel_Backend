e2b6b0158064bd9a97a6d9f6a8d68b79afe02cd5
“AndreuMartinez” <andreu.martinez.garcia.1992@gmail.com>
2022-04-10 12:03:14 +0200

    fix: corregido error de falta de campos al añadir campos en el registro del usuario

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -20,6 +20,7 @@ class AuthController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6',
+            'role' => 'required|min:4',
         ]);
 
         if($validator->fails()){
