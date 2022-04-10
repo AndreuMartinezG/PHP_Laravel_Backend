1031236560a409fa4533b87fd5071a8de4a12f05
“AndreuMartinez” <andreu.martinez.garcia.1992@gmail.com>
2022-04-10 19:19:52 +0200

    feat: Creado model para Messages y relaciones con otras tablas

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     use HasFactory;
+
+    public $fillable = [
+        'message',
+        'date',
+        'FromPlayer',
+        'PartyID'
+    ];
+
+    public function players()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function parties()
+    {
+        return $this->belongsTo(Party::class);
+    }
 }
