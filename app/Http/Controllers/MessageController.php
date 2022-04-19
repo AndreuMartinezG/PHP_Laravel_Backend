193d28f1996d136fb7a7c4e4ea2d909372cacc10
“AndreuMartinez” <andreu.martinez.garcia.1992@gmail.com>
2022-04-19 11:28:20 +0200

    fix: error al importar AppHttpControllersMessage

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 use App\Models\Party;
+use App\Models\Message;
 use App\Models\User;
 
 class MessageController extends Controller
